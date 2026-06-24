caec648a40d69e98afb8c1de0742a93c09b42eb9
Kunal <kunalmaheshwari26@gmail.com>
2026-06-24 23:30:01 +0530

    Project initialization

---
+++ src/main/java/com/practise/java/fulfillguard/FulfillguardApplication.java
@@ -0,0 +1,13 @@
+package com.practise.java.fulfillguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/java/fulfillguard/FulfillguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.java.fulfillguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
