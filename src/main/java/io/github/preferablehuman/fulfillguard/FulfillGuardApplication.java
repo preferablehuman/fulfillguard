f1e4563d693bcfece8c883e1d174536f17d7d6df
Kunal <kunalmaheshwari26@gmail.com>
2026-06-26 00:49:08 +0530

    updated the pom for correct groupid, corrected the name of the main class as per standards

--- src/main/java/io/github/preferablehuman/fulfillguard/FulfillGuardApplication.java
+++ src/main/java/io/github/preferablehuman/fulfillguard/FulfillGuardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FulfillguardApplication {
+public class FulfillGuardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FulfillguardApplication.class, args);
+		SpringApplication.run(FulfillGuardApplication.class, args);
 	}
 
 }
